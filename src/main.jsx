993b761fb5213ffacc7388d3797ee1a55e52a8e0
Macbook <macbook@Catherines-MBP.lan>
2024-04-13 00:33:57 -0400

    Add the new edit route.

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import Root, { loader as rootLoader, action as rootAction,
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader, } from "./routes/contact";
+import EditContact from "./routes/edit";
+
 
 
 const router = createBrowserRouter([
@@ -23,6 +25,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
