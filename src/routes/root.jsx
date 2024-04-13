f40d0204f5b28df130b0cec30acc29b3368bce81
Macbook <macbook@Catherines-MBP.lan>
2024-04-13 14:08:11 -0400

    Fixed sidebar issue

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -64,18 +64,18 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                                    <NavLink
-                    to={`contacts/${contact.id}`}
-                    className={({ isActive, isPending }) =>
-                      isActive
-                        ? "active"
-                        : isPending
-                        ? "pending"
-                        : ""
-                    }
-                  >
-                    {/* other code */}
-                  </NavLink>
+<NavLink to={`contacts/${contact.id}`} className={({ isActive, isPending }) =>
+                                        isActive ? "active" : isPending ? "pending" : ""
+                                    }>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
                 </li>
               ))}
             </ul>
